bdae169e1c5dab3fe5f01bd679862fbd7ed888da
Hank Tom <hanktom@gmail.com>
2021-09-01 20:50:27 +0800

    game layout

--- app/src/main/java/com/tom/numberguess/MainActivity.kt
+++ app/src/main/java/com/tom/numberguess/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.tom.numberguess
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -11,24 +12,27 @@ import android.view.Menu
 import android.view.MenuItem
 import com.tom.numberguess.databinding.ActivityMainBinding
 
+//Controller
 class MainActivity : AppCompatActivity() {
 
+    private val TAG: String = MainActivity::class.java.simpleName
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d(TAG, "onCreate: ")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
-
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        binding.contentMain.min.text = "1"
+        binding.contentMain.max.text = "100"
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -42,9 +46,41 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                Log.d(TAG, "onOptionsItemSelected: ")
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
 }
